96787f588a9c730a74926a14f3edd7bcf25d10fa
hiratara <hiratara@cpan.org>
2013-12-05 22:50:03 +0900

    Faild to implement the 5th day

--- src/Doukaku/UpDownButton.hs
+++ src/Doukaku/UpDownButton.hs
@@ -1,2 +1,26 @@
-module Doukaku.UpDownButton (solve) where
-solve = id
+module Doukaku.UpDownButton  where
+
+import Numeric.Lens (decimal, binary)
+import Control.Lens (preview, review)
+
+toBool :: Char -> Bool
+toBool '0' = False
+toBool _   = True
+
+toBinary :: String -> [Bool]
+toBinary s = maybe [] id $ do
+  binstr <- preview decimal s :: Maybe Int
+  Just . map toBool . review binary $ binstr
+
+count :: [Bool] -> Int
+count = (\n -> n - 1) . countT 0 0
+  where
+    truesCost precost n = min (n * 2) (n + 3 - precost)
+    countT precost acum [] = truesCost precost acum
+    countT precost acum (True :xs) = countT precost (acum + 1) xs
+    countT precost acum xs@(False:_) = truesCost precost acum + countF xs
+    countF [] = 0
+    countF xs@(True:_) = countT 1 0 xs
+    countF (False:xs) = 1 + countF xs
+solve :: String -> String
+solve = show . count . toBinary
