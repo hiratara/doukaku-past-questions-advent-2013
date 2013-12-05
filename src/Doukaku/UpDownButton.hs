e9f500e93612217defc1da0104ab4486cacc8816
hiratara <hiratara@cpan.org>
2013-12-05 23:09:54 +0900

    Implement the 5th day

--- src/Doukaku/UpDownButton.hs
+++ src/Doukaku/UpDownButton.hs
@@ -1,2 +1,15 @@
 module Doukaku.UpDownButton (solve) where
-solve = id
+import Data.Set (Set, empty, singleton, member, union, elems, fromList, difference)
+
+solve :: String -> String
+solve = show . minope empty . singleton . read
+
+minope :: Set Int -> Set Int -> Int
+minope done new'
+  |   1  `member` new' = 1
+  | (-1) `member` new' = 1
+  | otherwise = 1 + minope (done `union` new') (new `difference` done)
+  where
+    new = fromList $ do
+      x <- elems new'
+      (if x `mod` 2 == 0 then [x `div` 2] else []) ++ [x + 1, x - 1]
