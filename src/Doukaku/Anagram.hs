1cb45f393987b09b64ae47d6a5d14e66cb00a08e
hiratara <hiratara@cpan.org>
2013-12-08 02:14:11 +0900

    Implement the 8th day

--- src/Doukaku/Anagram.hs
+++ src/Doukaku/Anagram.hs
@@ -1,3 +1,20 @@
 module Doukaku.Anagram (solve) where
 
-solve = id
+solve :: String -> String
+solve = show . maximum . map anagramLen' . allTails
+  where
+    anagramLen' ([], _) = 0
+    anagramLen' (y:ys, xs) = max (anagramLen (y:ys) xs) (anagramLen ys xs + 1)
+
+anagramLen :: String -> String -> Int
+anagramLen [] _ = 0
+anagramLen (x:xs) ys = max result (anagramLen xs ys)
+  where
+    (_, ys2') = break (== x) ys
+    result = if null ys2' then 0 else 2 + anagramLen xs (tail ys2')
+
+allTails :: String -> [(String, String)]
+allTails = allTails' []
+  where
+    allTails' ys [] = (ys, []):[]
+    allTails' ys (x:xs) = (ys, x:xs) : allTails' (x:ys) xs
