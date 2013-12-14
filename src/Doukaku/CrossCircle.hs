665a95391e33762154525bec9c5f01e27689eb84
hiratara <hiratara@cpan.org>
2013-12-14 12:51:01 +0900

    Implement the 14th day

--- src/Doukaku/CrossCircle.hs
+++ src/Doukaku/CrossCircle.hs
@@ -1,3 +1,23 @@
 module Doukaku.CrossCircle (solve) where
 
-solve = id
+solve :: String -> String
+solve = show . (`div` 2) . sum . map (uncurry countPoints) . searchLines
+
+countPoints :: Eq a => [a] -> [a] -> Int
+countPoints [] _ = 0
+countPoints (x:xs) ys = (+ countPoints xs ys) . length . filter (== x) $ ys
+
+searchLines :: Eq a => [a] -> [([a], [a])]
+searchLines = searchLines' []
+
+searchLines' :: Eq a => [a] -> [a] -> [([a], [a])]
+searchLines' _ [] = []
+searchLines' ds (x:xs) = (searchLines'' [] x xs ds) ++ searchLines' (x:ds) xs
+
+searchLines'' :: Eq a => [a] -> a -> [a] -> [a] -> [([a], [a])]
+searchLines'' _ _ [] _ = []
+searchLines'' ds t0 (t:ts) cp
+  | t == t0   = (ds, ts ++ cp) : tails
+  | otherwise = tails
+  where
+    tails = searchLines'' (t:ds) t0 ts cp
