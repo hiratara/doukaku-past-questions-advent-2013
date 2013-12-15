58b18fde6d03accc89bfd03456408789bbbaa0b4
hiratara <hiratara@cpan.org>
2013-12-15 15:04:40 +0900

    Better implementation

--- src/Doukaku/Entropy.hs
+++ src/Doukaku/Entropy.hs
@@ -1,42 +1,56 @@
 module Doukaku.Entropy (solve) where
+import Numeric.Lens (hex, binary)
+import Control.Lens (review, preview)
 
 solve :: String -> String
 solve = decrypt . concatMap parse
 
-parse :: Char -> String
-parse '0' = "0000"
-parse '1' = "1000"
-parse '2' = "0100"
-parse '3' = "1100"
-parse '4' = "0010"
-parse '5' = "1010"
-parse '6' = "0110"
-parse '7' = "1110"
-parse '8' = "0001"
-parse '9' = "1001"
-parse 'a' = "0101"
-parse 'b' = "1101"
-parse 'c' = "0011"
-parse 'd' = "1011"
-parse 'e' = "0111"
-parse 'f' = "1111"
+data BDD = Leaf Char | Done | Undefined | Node BDD BDD deriving (Eq, Show)
+
+define :: [Char] -> Char -> BDD -> BDD
+define [] c Undefined = if c == '\0' then Done else Leaf c
+define ('1':bs) c bdd = case bdd of
+  Undefined -> Node Undefined (define bs c Undefined)
+  Node f t -> Node f (define bs c t)
+define ('0':bs) c bdd = case bdd of
+  Undefined -> Node (define bs c Undefined) Undefined
+  Node f t -> Node (define bs c f) t
+define _ c _ = error ("Bad definition of " ++ [c])
+
+codebdd :: BDD
+codebdd = define "000"     't'
+        $ define "0010"    's'
+        $ define "0011"    'n'
+        $ define "0100"    'i'
+        $ define "01010"   'd'
+        $ define "0101101" 'c'
+        $ define "010111"  'l'
+        $ define "0110"    'o'
+        $ define "0111"    'a'
+        $ define "10"      'e'
+        $ define "1100"    'r'
+        $ define "1101"    'h'
+        $ define "111"     '\0'
+        $ Undefined
 
 decrypt :: String -> String
-decrypt input = maybe "*invalid*" id (decrypt' input)
+decrypt is = maybe "*invalid*" answer $ decrypt' codebdd is
   where
-  decrypt' :: String -> Maybe String
-  decrypt' ('0':'0':'0':xs) = fmap ('t' :) $ decrypt' xs
-  decrypt' ('0':'0':'1':'0':xs)  = fmap ('s' :) $ decrypt' xs
-  decrypt' ('0':'0':'1':'1':xs)  = fmap ('n' :) $ decrypt' xs
-  decrypt' ('0':'1':'0':'0':xs) = fmap ('i' :) $ decrypt' xs
-  decrypt' ('0':'1':'0':'1':'0':xs) = fmap ('d' :) $ decrypt' xs
-  decrypt' ('0':'1':'0':'1':'1':'0':'1':xs) = fmap ('c' :) $ decrypt' xs
-  decrypt' ('0':'1':'0':'1':'1':'1':xs) = fmap ('l' :) $ decrypt' xs
-  decrypt' ('0':'1':'1':'0':xs) = fmap ('o' :) $ decrypt' xs
-  decrypt' ('0':'1':'1':'1':xs) = fmap ('a' :) $ decrypt' xs
-  decrypt' ('1':'0':xs) = fmap ('e' :) $ decrypt' xs
-  decrypt' ('1':'1':'0':'0':xs) = fmap ('r' :) $ decrypt' xs
-  decrypt' ('1':'1':'0':'1':xs) = fmap ('h' :) $ decrypt' xs
-  decrypt' ('1':'1':'1':xs) = Just $ ':' : show (length input - length xs)
-  decrypt' _ = Nothing
+    answer (n, s) = s ++ ':' : show n
 
+decrypt' :: BDD -> String -> Maybe (Int, String)
+decrypt' Done _ = Just (0, [])
+decrypt' (Leaf c)  xs = do
+  (n', cs) <- decrypt' codebdd xs
+  return (n', c:cs)
+decrypt' (Node f _) ('0':xs) = do
+  (n', cs) <- decrypt' f xs
+  return (n' + 1, cs)
+decrypt' (Node _ t) ('1':xs) = do
+  (n', cs) <- decrypt' t xs
+  return (n' + 1, cs)
+decrypt' _ _ = Nothing
+
+parse :: Char -> String
+parse c = reverse . tail . review binary $ n
+  where n = maybe 0 id . preview hex $ '1':c:[] :: Int
