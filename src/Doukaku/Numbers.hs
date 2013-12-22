706e07012d1617f5086601a85d804d1d07925dda
hiratara <hiratara@cpan.org>
2013-12-22 17:44:12 +0900

    Implement the 22th day

--- src/Doukaku/Numbers.hs
+++ src/Doukaku/Numbers.hs
@@ -1,3 +1,18 @@
 module Doukaku.Numbers (solve) where
-
-solve = id
+import Data.List (sort)
+solve :: String -> String
+solve input = let ns' = drop (read order - 1) . uniq . sort $ ds
+              in if null ns' then "-" else head ns'
+  where
+    (order, _:is) = break (== ':') input
+    is' = concatMap inflate . zip is $ [(0 :: Int)..]
+      where
+        inflate (d, n)
+          | d `elem` "69" = [('6', n), ('9', n)]
+          | otherwise = [(d, n)]
+    ds = [[d1, d2, d3, d4] | (d1, n1) <- is', d1 /= '0',
+                             (d2, n2) <- is', n2 /= n1,
+                             (d3, n3) <- is', n3 `notElem` [n1, n2],
+                             (d4, n4) <- is', n4 `notElem` [n1, n2, n3]]
+    uniq (x:y:ys) = (if x == y then id else (x :)) . uniq $ (y:ys)
+    uniq xs = xs
