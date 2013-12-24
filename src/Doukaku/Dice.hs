a876492d414762e49adfd644a119bfdbb0fbb39b
hiratara <hiratara@cpan.org>
2013-12-24 21:36:56 +0900

    Implement of eve

--- src/Doukaku/Dice.hs
+++ src/Doukaku/Dice.hs
@@ -1,3 +1,14 @@
 module Doukaku.Dice (solve) where
+import Data.Char (intToDigit, digitToInt)
 
-solve = id
+solve :: String -> String
+solve = map head . scanl (flip roll) "153"
+
+roll :: Char -> String -> String
+roll 'E' (x:y:z:_) = z:y:(rev x):[]
+roll 'W' ds = let e = roll 'E' in e . e . e $ ds
+roll 'S' ds = let n = roll 'N' in n . n . n $ ds
+roll 'N' (x:y:z:_) = y:(rev x):z:[]
+
+rev :: Char -> Char
+rev = intToDigit . (7 -) . digitToInt
